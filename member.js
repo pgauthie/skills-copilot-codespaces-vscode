c0e01d57b083bda4c2123626454d08039c9e150b
Pascal Gauthier <101276551+pgauthie@users.noreply.github.com>
2024-05-10 10:21:53 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,27 @@
+function skillsMember() {
+    var data = {
+        "skills": [
+            {
+                "name": "HTML",
+                "level": "80%"
+            },
+            {
+                "name": "CSS",
+                "level": "70%"
+            },
+            {
+                "name": "JavaScript",
+                "level": "60%"
+            },
+            {
+                "name": "PHP",
+                "level": "50%"
+            },
+            {
+                "name": "SQL",
+                "level": "40%"
+            }
+        ]
+    };
+    return data;
+}
